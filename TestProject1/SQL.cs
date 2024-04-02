61d3ea7eebaf32048ad81941b5a426208868b0fe
Tomas1974 <114471994+Tomas1974@users.noreply.github.com>
2024-04-02 15:50:15 +0200

    Update

---
+++ TestProject1/SQL.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using ConsoleApp1;
+using NUnit.Framework;
+using Npgsql;
+
+namespace NpgsqlTests
+{
+    [TestFixture]
+    public class HelperTests
+    {
+        
+        
+        
+            [Test]
+            public void HelperClass_Initializes_Successfully()
+            {
+        
+            
+                Assert.DoesNotThrow(() => {
+                    var conn = Helper.DataSource.OpenConnection();
+                    Assert.IsNotNull(conn);
+                    conn.Close();
+                }, "Expected the Helper class to initialize and open a connection successfully, but it did not.");
+            }
+                
+        }
+
+       
+    }
+}

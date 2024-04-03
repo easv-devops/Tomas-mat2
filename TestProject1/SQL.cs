54a3ef3eeb62c3c1fad513f16580a9ccb4466afb
Tomas1974 <114471994+Tomas1974@users.noreply.github.com>
2024-04-03 10:41:02 +0200

    Update

--- TestProject1/SQL.cs
+++ TestProject1/SQL.cs
@@ -10,21 +10,8 @@ namespace NpgsqlTests
     {
         
         
-        
-            [Test]
-            public void HelperClass_Initializes_Successfully()
-            {
-        
-            
-                Assert.DoesNotThrow(() => {
-                    var conn = Helper.DataSource.OpenConnection();
-                    Assert.IsNotNull(conn);
-                    conn.Close();
-                }, "Expected the Helper class to initialize and open a connection successfully, but it did not.");
-            }
                 
         }
 
        
     }
-}
